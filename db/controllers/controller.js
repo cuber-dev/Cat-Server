01442b214fbfffc3ac417fced9428cbaad3acee4
anukitty <adepunandhu9@gmail.com>
2023-06-16 10:44:49 +0530

    updated controller-db

--- db/controllers/controller.js
+++ db/controllers/controller.js
@@ -1,20 +1,20 @@
 
 
 
-async function updateOne(collection,query) {
+async function updateOne(collection,{ query , action}) {
     try {
       const result = await collection.updateOne(query.filter, query.update);
-      console.log('updateOne')
+      console.log(action)
       return { message: 'updateOne action successful' };
     } catch (error) {
       console.log(error);
     }
   }
   
-  async function read(collection,{ query }) {
+  async function read(collection,{ query , action }) {
     try {
       const result = await collection.find(query || {}).toArray();
-      console.log('read')
+      console.log(action)
       return { result, message: 'read action successful' };
     } catch (error) {
       console.log(error);
